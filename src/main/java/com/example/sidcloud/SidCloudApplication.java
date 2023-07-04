5ad73a8c841ecd3abbf5e468cd3799425c926271
SiddheshD <siddeo85@gmail.com>
2023-07-04 19:40:36 +0530

    first commit hello

---
+++ src/main/java/com/example/sidcloud/SidCloudApplication.java
@@ -0,0 +1,14 @@
+package com.example.sidcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidCloudApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/test/java/com/example/sidcloud/SidCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
